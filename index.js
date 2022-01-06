854cf673173154e8aec6dbcbdda67ce5417317b1
emanalrashidi37 <emanalrashidi37@gmail.com>
2022-01-06 19:52:29 +0300

    finish dom bmi

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  // challenge1 
+
+  height = height / 100;
+  BMI = weight / (height * height);
+  
+  alert(BMI)
+
+  //challenge2
+  if(weight < 18.5){
+    alert("underwight")
+  }
+  else if ( weight >= 18.5 && weight >= 24.9){
+    alert("healthyweight");
+  }
+  else if ( weight >= 25 && weight <= 29.9){
+    alert("overweight");
+  }
+  else ( weight >= 30 )
+    alert("Above Obesity");
+  
+
 }
